c3604dd8b32d0460889405930c24a98fc98bc3be
Keyrun32-J <kj058006@gmail.com>
2025-06-17 23:49:57 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 
 # MongoDB connection string
-client = MongoClient("mongodb+srv://society_user:Bank@1980@cluster0.sy8c2a5.mongodb.net/")  # or your Mongo URI
+client = MongoClient("mongodb+srv://society_user:Bank%401980@cluster0.sy8c2a5.mongodb.net/")
 
 client = MongoClient(MONGO_URI)
 db = client["society_db"]
